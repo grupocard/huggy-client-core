bc4c36cea6cee0e4f7907ab87865d0fa0f3b5a30
Lucian Rezende <lucianfiocello@gmail.com>
2020-05-03 12:57:00 -0300

    New classes included

---
+++ source/Huggy.Client.Api/Huggy.Client.Api/Models/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Huggy.Client.Api.Models
+{
+    public class Contact
+    {
+        [DataContract]
+        public class agent
+        {
+
+            [DataMember(Name = "id")]
+            public int Id { get; set; }
+
+            [DataMember(Name = "name")]
+            public string Name { get; set; }
+
+            [DataMember(Name = "email")]
+            public object Email { get; set; }
+
+            [DataMember(Name = "mobile")]
+            public string Mobile { get; set; }
+
+            [DataMember(Name = "phone")]
+            public object Phone { get; set; }
+
+            [DataMember(Name = "customFields")]
+            public ContactCustomField CustomFields { get; set; }
+        }
+    }
+}
---
+++ source/Huggy.Client.Api/Huggy.Client.Api/Models/ContactCustomField.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Huggy.Client.Api.Models
+{
+    [DataContract]
+    public class ContactCustomField
+    {
+
+        [DataMember(Name = "codigo_sgv_customer")]
+        public string CodigoSgvCustomer { get; set; }
+
+        [DataMember(Name = "codigo_erp_customer")]
+        public object CodigoErpCustomer { get; set; }
+
+        [DataMember(Name = "cpf_customer")]
+        public string CpfCustomer { get; set; }
+    }
+}
